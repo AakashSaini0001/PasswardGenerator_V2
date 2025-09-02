1ad4bb8f4fdcd72753b0c7c51ee9ec32f6c378b1
Aakash Saini <130988822+AakashSaini0001@users.noreply.github.com>
2025-09-02 14:53:31 +0530

    Update initial.jsx
    imported dark mode

--- client/src/components/initial.jsx
+++ client/src/components/initial.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DarkMode from "./components/darkMode";
 
 export function Initial() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-green-100 via-green-200 to-green-300 p-4">
+      <DarkMode/>
       <div className="w-auto bg-white/20 backdrop-blur-lg rounded-2xl shadow-xl p-6 text-center border border-white/30">
         <h1 className="text-3xl font-bold text-black mb-4 drop-shadow">
           🔐 Welcome to the Password Generator!
