172bdfea3493e1b465e8fe19f5ca492489701ee7
Aakash Saini <130988822+AakashSaini0001@users.noreply.github.com>
2025-09-02 14:31:15 +0530

    Update vite.config.js
    revert the changes.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from "@tailwindcss/vite"; // Add this line
 export default defineConfig({
   darkMode: 'class',
   plugins: [react(), tailwindcss()], // Add tailwindcss() here
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 });
